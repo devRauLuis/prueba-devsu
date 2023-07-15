7ceb029df089085fefb117e65935f2faea17d853
Raul Luis <soyrauluis@gmail.com>
2023-07-14 22:02:22 -0400

    Update styles, format

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
